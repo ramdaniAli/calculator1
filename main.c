f32487cc4d922379c1baca82427d80a4cf03e311
Ali ramdani <ali.ramdani.dz@viacesi.fr>
2017-11-06 14:03:47 +0100

    commit

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int addition (int a, int b)
 }
 int main()
 {
-   printf("L’addition de a et de b vaut %d\n", addition(1,4));
-    return 0;
+printf("L’addition de a et de b vaut %d\n", addition(1,4));
+  return 0;
 }
