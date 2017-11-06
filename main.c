abddc6ee76458179581493a7dae9ee7b0263aac1
ramdaniAli <ali.ramdani.dz@viacesi.fr>
2017-11-06 12:24:33 +0100

    Revert "add main program"
    This reverts commit 9775f609af307459a414cc9aacc37b78aabb81d2.

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition (int a, int b)
-{
-    return a+b;
-}
 int main()
 {
-    printf("%d\n",addition(1+4));
+    printf("Hello world!\n");
     return 0;
 }
